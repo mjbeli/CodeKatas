fc4004495dbe3379b75c65501347050759393ead
mjbeli <mjba82@gmail.com>
2019-10-27 18:59:57 +0100

    Estructura sln: biblioteca y pruebas unitarias

---
+++ BankingKata/BankingCode.Test/AccountTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankingCode.Test
+{
+    [TestClass]
+    public class AccountTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(1==1);
+        }
+    }
+}
