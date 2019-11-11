5ed1a4c78934a88915a31a327ac41b722bedea56
mjbeli <mjba82@gmail.com>
2019-11-11 22:33:40 +0100

    explicación: ¿por qué una clase abstracta?

--- BankingKata/BankingCode/AbstractAccount.cs
+++ BankingKata/BankingCode/AbstractAccount.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Text;
 
+
+/*
+Why an abstract class? 
+The first version of this class wasn't abstract and implement all 3  of the inteface, but then I think...
+ ...ok, it isn't very good that every client must to implement these 3 methods in every case.
+ ... what if a client only want a different way to Print the last statement.
+ ... It's sound logical that Deposit and Withdraw methods remains, but PrintStatement can change very often!
+So.. Let's apply the ISP principe and use 2 different intefaces. It's a logical division!
+But I don't want to expose inner variables. That breaks encapsulation --> Tell don't ask, by Martin Fowler.
+So maybe a solution is to implement operations methods by default and let the clients decide how to
+implement the Print method.
+
+So here we are. I think this solution provide flexibility, encapsulation remains...
+...and, everybody can decide how to print the operations.
+ */
 namespace BankingCode
 {
     public abstract class AbstractAccount: IAccount
     {
-        protected int _currentAmount = 0;
+        protected int _currentAmount = 0; // protected, so the inherint classes can use this variables
         protected int _lastAmountModification = 0;
         public void Deposit(int amount)
         {
@@ -22,6 +37,6 @@ namespace BankingCode
             _currentAmount -= amount;
         }
         
-        public abstract string PrintStatement();       
+        public abstract string PrintStatement();  // Inherint classes must decide how to implement this.
     }
 }
