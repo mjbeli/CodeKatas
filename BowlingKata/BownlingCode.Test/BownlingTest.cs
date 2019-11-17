3c44f4840998890ca86d0717bdd926ad38890cdc
mjbeli <mjba82@gmail.com>
2019-11-17 12:28:43 +0100

    Primeras pruebas, inicio desarrollo.

--- BowlingKata/BownlingCode.Test/BownlingTest.cs
+++ BowlingKata/BownlingCode.Test/BownlingTest.cs
@@ -6,10 +6,27 @@ namespace BownlingCode.Test
     [TestClass]
     public class BownlingTest
     {
+        private IBowlingGame _myGame;
+
+        // Run before each test
+        [TestInitialize]
+        public void generateNewGame ()
+        {
+            _myGame = new BowlingGame();
+        }
+
         [TestMethod]
-        public void TestMethod1()
+        public void ShowBasicScore()
+        {
+            Assert.IsTrue(_myGame.score() == 0);            
+        }
+
+        [TestMethod]
+        public void NotAllowedPinsInASingleRollTest()
         {            
-            Assert.IsTrue(true);
+            _myGame.roll(12);
+            _myGame.roll(-3);
+            Assert.IsTrue(_myGame.score() == 0);
         }
     }
 }
--- BowlingKata/BownlingCode/BowlingGame.cs
+++ BowlingKata/BownlingCode/BowlingGame.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BownlingCode
 {
     public class BowlingGame: IBowlingGame
     {
+        private IList<int> _rolls = new List<int>(); // Tell, don't ask. Don't expose this atribute.
+
         public void roll(int pins)
         {
-            throw new NotImplementedException();
+            if(pins < 0 || pins > 10)
+                return;
         }
         
         public int score()
         {
-            throw new NotImplementedException();
+            if(_rolls.Count == 0)
+                return 0;
+
+            return -1;
         }
     }
 }
