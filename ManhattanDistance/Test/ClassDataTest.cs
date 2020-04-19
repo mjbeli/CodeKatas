9d4cca8173f9afcf64e1f34614ecd0180f9962cc
mjbeli <mjba82@gmail.com>
2020-04-19 11:37:22 +0200

    Lógica dentro de la clase Point.

--- ManhattanDistance/Test/ClassDataTest.cs
+++ ManhattanDistance/Test/ClassDataTest.cs
@@ -18,4 +18,71 @@ namespace Test.ClassData
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 
+
+    public class EqualPoints : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+
+            yield return new object[] {
+                new Point (1, 2),
+                new Point (1, 2)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class FirstPointGreaterThanSecond : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+
+            yield return new object[] {
+                new Point (3, 5),
+                new Point (1, 2)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class FirstPointLowerThanSecond : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+
+            yield return new object[] {
+                new Point (1, 2),
+                new Point (3, 5)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class FirstPointXGreaterThanSecondX : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+
+            yield return new object[] {
+                new Point (7, 2),
+                new Point (3, 5)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class FirstPointYLowerThanSecondY : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+
+            yield return new object[] {
+                new Point (4, 2),
+                new Point (3, 5)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    
+
 }
