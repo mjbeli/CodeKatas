26c7dae615698c8f10304f53c9fcd2a81037195e
mjbeli <mjba82@gmail.com>
2020-05-15 11:15:04 +0200

    first commit composite pattern

---
+++ CompositePattern/CompositeTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace CompositePattern.Test
+{
+    public class CompositePattern
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(1==1);
+        }
+    }
+}
